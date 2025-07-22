ec34c2fc0ba9abb518ba166c8423535d0f0c0bb6
MackenzieJosie <mackenzie.josie@gmail.com>
2025-07-22 01:23:34 -0400

    no more unsecret key

--- moodtracker/settings.py
+++ moodtracker/settings.py
@@ -23,7 +23,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1q54ecoc1vljbn6*@ul)c@^bqrtr^q3^8v@-3t8evl_-e^5ov8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
